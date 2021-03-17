fd1f62722e427e6a35e4403afb762047f83287f8
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-03-17 15:44:26 +0100

    Footer component done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,10 @@ import React from "react";
 import "../App.css";
 import HeroSection from "../components/HeroSection/HeroSection";
 import Cards from "../components/Cards/Cards";
+import Footer from "../components/Footer/Footer";
 
 function Home() {
-  return (
-    <>
-      <HeroSection />
-      <Cards />
-    </>
-  );
+    return (<> <HeroSection /> <Cards /> <Footer /> </>);
 }
 
 export default Home;
